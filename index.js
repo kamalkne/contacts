21ce4826ce3a3658ef31399c6c23ef3518e0daef
KOKNE Kamal <KKE6@dsone.3ds.com>
2017-01-06 19:56:42 +0530

    Created rest service (post) to upload vcf file.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var fs = require('fs');
 var vcard = require('vcard-json');
+var express = require('express');
+var busboy = require('connect-busboy');
+var path = require('path');
+
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
 
 function vcfTo (vcfFile) {
     this.vcfFile = vcfFile;
@@ -35,3 +47,26 @@ vcfTo.prototype.readVcf = function() {
 
 var v = new vcfTo('Contacts.vcf');
 v.readVcf();
+
+
+app.route('/postvcf')
+    .post(function (req, res, next) {
+
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            fstream = fs.createWriteStream(__dirname + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {
+                console.log("Upload Finished of " + filename);
+                res.redirect('back');           //where to go next
+            });
+        });
+    });
+
+app.post('/postvcf', function( req, res ) {
+
+})
